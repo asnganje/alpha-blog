59ee805ddb0876f27aabf27905c087efe45955cc
asnganje <abd.nganje@gmail.com>
2025-03-17 15:16:42 +0300

    added logic for displaying or show an article

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "pages#home"
   get "about", to: "pages#about"
+  # get "show", to: "pages#show"
+  resources :articles, only: [:show]
 
 end
