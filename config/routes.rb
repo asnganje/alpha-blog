5cb6b51ffd1d6813ab792753e626d5a30fee2a3d
asnganje <abd.nganje@gmail.com>
2025-03-17 13:49:13 +0300

    Add a table for data manipulation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
-  root "pagees#home"
-  get "about", to: "pagees#about"
+  root "pages#home"
+  get "about", to: "pages#about"
 
 end
