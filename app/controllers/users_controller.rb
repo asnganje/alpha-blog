1f65ae7a610175a2cc6918dea62897e8e85b9b09
asnganje <abd.nganje@gmail.com>
2025-03-21 17:43:02 +0300

    clean up layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users=User.all
+  end
+
   def show
     @user=User.find(params[:id])
     # @articles=@user.articles
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]= "#{@user.username}'s details successfully updated"
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end
