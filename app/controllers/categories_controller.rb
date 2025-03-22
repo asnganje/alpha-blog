f4626d1885a3abdb498708437e45fc13b919722a
asnganje <abd.nganje@gmail.com>
2025-03-22 14:11:41 +0300

    add category model and controller logic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,43 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+  before_action :require_admin, except: [:index, :show]
+
   def new
+    @category=Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      flash[:notice] = "Category created successfully!"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def show
   end
 
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 2)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(helpers.logged_in? && helpers.current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 
 end
